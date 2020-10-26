1b44827b24aea496da18a26d4cf401978f85fd13
Shazib Rahman <shazibrahman@outlook.com>
2020-10-26 21:06:09 +0530

    Added comments

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
 from FireflyAlgorithm import *
 
 
-def Fun(D, sol):
+def function(D, sol):
     val = 0.0
     for i in range(D):
         val = val + sol[i] * sol[i]
     return val
 
 
-Algorithm = FireflyAlgorithm(10, 7, 100, 0.5, 0.2, 1.0, 120, 180, Fun)
+
+Algorithm = FireflyAlgorithm(10, 7, 100, 0.5, 0.2, 1.0, 120, 180, function)
 Best = Algorithm.Run()
 
 print (Best)
