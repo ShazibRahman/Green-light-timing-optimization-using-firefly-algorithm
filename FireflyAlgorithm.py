6f2120997f503621778132cd653693bd62915768
Shazib Rahman <shazibrahman@outlook.com>
2020-10-27 15:25:08 +0530

    added Noise for better movement and hence solution

--- FireflyAlgorithm.py
+++ FireflyAlgorithm.py
@@ -33,8 +33,8 @@ class FireflyAlgorithm():
         for i in range(self.NP):
             for j in range(self.D):
                 self.Fireflies[i][j] = random.uniform(
-                    self.LB, self.UB) +self.Fireflies[i][j]
-            self.Fitness[i]=1
+                    20, 50) +self.Fireflies[i][j] # adding noise of 20-30 for better movement and hence better Solution
+            self.Fitness[i]=random.uniform(30,60)
 
             self.I[i] = self.Fitness[i]
 
